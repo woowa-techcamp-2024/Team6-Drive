e0d7fbc27819d7ed287f7b7e7f4dadc25745e6ba
KoKimss <seungsu8848@ajou.ac.kr>
2024-08-10 21:24:16 +0900

    refactor : 객체 생성 팩토리 메소드로 변경
    - User, FolderMetadataFactory 생성 팩토리 메소드로 변경

--- src/main/java/com/woowacamp/storage/domain/user/service/UserService.java
+++ src/main/java/com/woowacamp/storage/domain/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.woowacamp.storage.domain.user.service;
 
+import static com.woowacamp.storage.domain.folder.entity.FolderMetadataFactory.*;
+import static com.woowacamp.storage.domain.user.entity.UserFactory.*;
+
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
@@ -8,9 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.woowacamp.storage.domain.folder.entity.FolderMetadata;
 import com.woowacamp.storage.domain.folder.repository.FolderMetadataRepository;
+import com.woowacamp.storage.domain.user.dto.UserDto;
+import com.woowacamp.storage.domain.user.dto.request.CreateUserReqDto;
 import com.woowacamp.storage.domain.user.entity.User;
-import com.woowacamp.storage.domain.user.entity.dto.UserDto;
-import com.woowacamp.storage.domain.user.entity.dto.request.CreateUserReqDto;
 import com.woowacamp.storage.domain.user.repository.UserRepository;
 import com.woowacamp.storage.global.error.CustomException;
 
@@ -18,44 +21,31 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class UserService {
 
 	private static final String rootFolderName = "rootFolder";
 	private final UserRepository userRepository;
 	private final FolderMetadataRepository folderMetadataRepository;
 
+	@Transactional(readOnly = true)
 	public UserDto findById(Long userId) {
 		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new CustomException(HttpStatus.BAD_REQUEST, "잘못된 id 입니다"));
+			.orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."));
 
-		return UserDto.builder()
-			.userName(user.getUserName())
-			.rootFolderId(user.getRootFolderId())
-			.id(user.getId()).build();
+		return new UserDto(user.getId(), user.getRootFolderId(), user.getUserName());
 	}
 
-	@Transactional(readOnly = false)
+	@Transactional
 	public UserDto save(CreateUserReqDto req) {
 		LocalDateTime now = LocalDateTime.now();
 
-		FolderMetadata rootFolder = folderMetadataRepository.save(FolderMetadata.builder()
-			.createdAt(now)
-			.updatedAt(now)
-			.uploadFolderName(rootFolderName)
-			.build());
+		FolderMetadata rootFolder = folderMetadataRepository.save(createFolderMetadata(now, rootFolderName));
 
-		User user = userRepository.save(User.builder()
-			.userName(req.getUserName())
-			.rootFolderId(rootFolder.getId())
-			.build());
+		User user = userRepository.save(createUser(req, rootFolder));
 
 		rootFolder.initOwnerId(user.getId());
 		rootFolder.initCreatorId(user.getId());
 
-		return UserDto.builder()
-			.rootFolderId(user.getRootFolderId())
-			.userName(user.getUserName())
-			.id(user.getId()).build();
+		return new UserDto(user.getId(), user.getRootFolderId(), user.getUserName());
 	}
 }
