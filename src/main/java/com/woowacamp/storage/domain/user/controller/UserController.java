f78ef0790e451f5c2b336fa3221b4c85a05e5754
KoKimss <seungsu8848@ajou.ac.kr>
2024-08-10 21:27:31 +0900

    refactor : 컨트롤러 반환 객체 변경
    - ResponseEntity -> dto 로 변경

--- src/main/java/com/woowacamp/storage/domain/user/controller/UserController.java
+++ src/main/java/com/woowacamp/storage/domain/user/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.woowacamp.storage.domain.user.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.woowacamp.storage.domain.user.entity.dto.UserDto;
-import com.woowacamp.storage.domain.user.entity.dto.request.CreateUserReqDto;
+import com.woowacamp.storage.domain.user.dto.UserDto;
+import com.woowacamp.storage.domain.user.dto.request.CreateUserReqDto;
 import com.woowacamp.storage.domain.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,15 +22,14 @@ public class UserController {
 
 	private final UserService userService;
 
-	@GetMapping
-	@RequestMapping("/{userId}")
-	public ResponseEntity<UserDto> getUser(@PathVariable Long userId) {
-		return ResponseEntity.ok(userService.findById(userId));
+	@GetMapping("/{userId}")
+	public UserDto getUser(@PathVariable Long userId) {
+		return userService.findById(userId);
 	}
 
 	@PostMapping
-	@RequestMapping
-	public ResponseEntity<UserDto> createUser(@RequestBody CreateUserReqDto req) {
-		return ResponseEntity.ok(userService.save(req));
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public UserDto createUser(@RequestBody CreateUserReqDto req) {
+		return userService.save(req);
 	}
 }
