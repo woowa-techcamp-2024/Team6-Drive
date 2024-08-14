9a199b236ffef2072941123380b4ed125aefc06a
kariskan <zkfzpf56@naver.com>
2024-08-14 13:05:08 +0900

    feat: exception debug message 출력

--- src/main/java/com/woowacamp/storage/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/woowacamp/storage/global/error/GlobalExceptionHandler.java
@@ -6,12 +6,18 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.woowacamp.storage.global.response.ErrorResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
 		ErrorResponse errorResponse = ErrorResponse.of(e.getHttpStatus(), e.getMessage());
+		if (e.getDebugMessage() != null) {
+			log.debug(e.getDebugMessage());
+		}
 		return ResponseEntity.status(e.getHttpStatus()).body(errorResponse);
 	}
 }
