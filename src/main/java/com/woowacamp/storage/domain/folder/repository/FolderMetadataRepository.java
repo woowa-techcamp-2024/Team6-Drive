b7bae03a14f3bd70ebc77d4fce51085a7097c00f
kariskan <zkfzpf56@naver.com>
2024-08-14 13:05:23 +0900

    feat: file, folder 관련 쿼리 추가

--- src/main/java/com/woowacamp/storage/domain/folder/repository/FolderMetadataRepository.java
+++ src/main/java/com/woowacamp/storage/domain/folder/repository/FolderMetadataRepository.java
@@ -1,8 +1,21 @@
 package com.woowacamp.storage.domain.folder.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.woowacamp.storage.domain.folder.entity.FolderMetadata;
 
 public interface FolderMetadataRepository extends JpaRepository<FolderMetadata, Long> {
+
+	boolean existsByIdAndCreatorId(Long id, Long creatorId);
+
+	boolean existsByParentFolderIdAndUploadFolderName(Long parentFolderId, String uploadFolderName);
+
+	@Query(value = """
+			select f.parentFolderId from FolderMetadata f where f.id = :id
+		""")
+	Optional<Long> findParentFolderIdById(@Param("id") Long id);
 }
