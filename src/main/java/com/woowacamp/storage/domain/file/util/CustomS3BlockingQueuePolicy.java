c6dcff94b913cadc393eb254eb78c6a475c2722d
kariskan <zkfzpf56@naver.com>
2024-08-14 12:59:35 +0900

    feat: OOM 방지를 위한 스레드 풀 대기 큐 정책 추가

---
+++ src/main/java/com/woowacamp/storage/domain/file/util/CustomS3BlockingQueuePolicy.java
@@ -0,0 +1,25 @@
+package com.woowacamp.storage.domain.file.util;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * S3에 파일 쓰기 작업을 하는
+ */
+public class CustomS3BlockingQueuePolicy implements RejectedExecutionHandler {
+
+	@Override
+	public void rejectedExecution(Runnable runnable, ThreadPoolExecutor executor) {
+		BlockingQueue<Runnable> queue = executor.getQueue();
+		try {
+			// 큐에 공간이 생길 때까지 무기한 대기
+			queue.put(runnable);
+		} catch (InterruptedException e) {
+			// 인터럽트 발생 시 현재 스레드의 인터럽트 상태를 설정
+			Thread.currentThread().interrupt();
+			throw new RejectedExecutionException("Task interrupted while waiting for queue space", e);
+		}
+	}
+}
