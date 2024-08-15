f52b8259ffac0373fe17f0afbbb4c324fdd388c9
kariskan <zkfzpf56@naver.com>
2024-08-15 14:59:49 +0900

    feat: 500 error 추가

--- src/main/java/com/woowacamp/storage/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/woowacamp/storage/global/error/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.woowacamp.storage.global.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +13,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GlobalExceptionHandler {
 
+	private static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
 		ErrorResponse errorResponse = ErrorResponse.of(e.getHttpStatus(), e.getMessage());
@@ -19,5 +22,12 @@ public class GlobalExceptionHandler {
 			log.debug(e.getDebugMessage());
 		}
 		return ResponseEntity.status(e.getHttpStatus()).body(errorResponse);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleException(Exception e) {
+		log.error(INTERNAL_SERVER_ERROR, e);
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR);
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
 	}
 }
