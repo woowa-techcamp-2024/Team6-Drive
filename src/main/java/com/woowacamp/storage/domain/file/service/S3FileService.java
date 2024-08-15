b97c178b27498be4afebb5a5d7ff3e87edb4f044
kariskan <zkfzpf56@naver.com>
2024-08-15 14:59:38 +0900

    fix: file size validation 추가

--- src/main/java/com/woowacamp/storage/domain/file/service/S3FileService.java
+++ src/main/java/com/woowacamp/storage/domain/file/service/S3FileService.java
@@ -2,17 +2,17 @@ package com.woowacamp.storage.domain.file.service;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.woowacamp.storage.domain.file.dto.FileMetadataDto;
+import com.woowacamp.storage.domain.file.dto.FormMetadataDto;
 import com.woowacamp.storage.domain.file.dto.PartContext;
+import com.woowacamp.storage.domain.file.dto.UploadState;
 import com.woowacamp.storage.domain.file.entity.FileMetadata;
 import com.woowacamp.storage.domain.file.entity.FileMetadataFactory;
 import com.woowacamp.storage.domain.file.repository.FileMetadataRepository;
@@ -29,7 +29,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class S3FileService implements FileService {
 
-	private static final Logger log = LoggerFactory.getLogger(S3FileService.class);
 	private final FileMetadataRepository fileMetadataRepository;
 	private final FolderMetadataRepository folderMetadataRepository;
 	private final UserRepository userRepository;
@@ -45,51 +44,25 @@ public class S3FileService implements FileService {
 	 * 사용자의 요청 데이터에 있는 사용자 정보, 상위 폴더 정보, 파일 사이즈의 정보를 저장
 	 */
 	@Transactional
-	public FileMetadataDto createInitialMetadata(Map<String, String> formFields, PartContext partContext) {
-		long userId;
-		long parentFolderId;
-		long fileSize;
-		try {
-			userId = Long.parseLong(formFields.get("userId"));
-			parentFolderId = Long.parseLong(formFields.get("parentFolderId"));
-			fileSize = Long.parseLong(formFields.get("fileSize"));
-		} catch (NumberFormatException exception) {
-			throw ErrorCode.INVALID_INPUT_VALUE.baseException();
-		}
-
+	public FileMetadataDto createInitialMetadata(FormMetadataDto formMetadataDto, PartContext partContext) {
 		String fileName = partContext.getCurrentFileName();
 		String fileType = getFileTypeByFileName(fileName);
-		User user = userRepository.findById(userId).orElseThrow(ErrorCode.USER_NOT_FOUND::baseException);
-
-		validateFileSize(fileSize, user.getRootFolderId());
-		validateFile(partContext, parentFolderId, fileName, fileType);
-		validateParentFolder(parentFolderId, userId);
+		User user = userRepository.findById(formMetadataDto.getUserId())
+			.orElseThrow(ErrorCode.USER_NOT_FOUND::baseException);
+		validateRequest(formMetadataDto, partContext, user, fileName, fileType);
 
 		String uuidFileName = getUuidFileName();
-		log.info("uuidFileName: {}", uuidFileName);
 
 		// 1차 메타데이터 생성
 		// TODO: 공유 기능이 생길 때, creatorId, ownerId 따로
 		FileMetadata fileMetadata = fileMetadataRepository.save(
-			FileMetadataFactory.buildInitialMetadata(user, parentFolderId, fileSize, uuidFileName, fileName, fileType));
+			FileMetadataFactory.buildInitialMetadata(user, formMetadataDto.getParentFolderId(),
+				formMetadataDto.getFileSize(), uuidFileName, fileName, fileType));
 
 		return FileMetadataDto.of(fileMetadata);
 	}
 
-	private void validateFileSize(long fileSize, Long rootFolderId) {
-		FolderMetadata rootFolderMetadata = folderMetadataRepository.findById(rootFolderId)
-			.orElseThrow(ErrorCode.FOLDER_NOT_FOUND::baseException);
-
-		if (fileSize > MAX_FILE_SIZE) {
-			throw ErrorCode.EXCEED_MAX_FILE_SIZE.baseException();
-		}
-		if (rootFolderMetadata.getSize() + fileSize > MAX_STORAGE_SIZE) {
-			throw ErrorCode.EXCEED_MAX_STORAGE_SIZE.baseException();
-		}
-	}
-
 	/**
-	 *
 	 * 사용자 정보는 로그인을 했다고 가정하고 사용했습니다.
 	 */
 	@Transactional
@@ -106,6 +79,40 @@ public class S3FileService implements FileService {
 		fileMetadata.updateUpdatedAt(now);
 	}
 
+	/**
+	 *
+	 * 요청 폼 데이터의 fieldFileSize와 실제 파일 크기인 uploadFileSize가 치일한 지 확인하는 메소드
+	 */
+	public void checkMetadata(UploadState state) {
+		long fieldFileSize = state.getFileMetadataDto().fileSize();
+		long uploadedFileSize = state.getFileSize();
+		if (fieldFileSize != uploadedFileSize) {
+			throw ErrorCode.INVALID_FILE_SIZE.baseException();
+		}
+	}
+
+	private void validateRequest(FormMetadataDto formMetadataDto, PartContext partContext, User user, String fileName,
+		String fileType) {
+		validateFileSize(formMetadataDto.getFileSize(), user.getRootFolderId());
+		validateFile(partContext, formMetadataDto.getParentFolderId(), fileName, fileType);
+		validateParentFolder(formMetadataDto.getParentFolderId(), formMetadataDto.getUserId());
+	}
+
+	private void validateFileSize(long fileSize, Long rootFolderId) {
+		FolderMetadata rootFolderMetadata = folderMetadataRepository.findById(rootFolderId)
+			.orElseThrow(ErrorCode.FOLDER_NOT_FOUND::baseException);
+
+		if (fileSize > MAX_FILE_SIZE) {
+			throw ErrorCode.EXCEED_MAX_FILE_SIZE.baseException();
+		}
+		if (rootFolderMetadata.getSize() + fileSize > MAX_STORAGE_SIZE) {
+			throw ErrorCode.EXCEED_MAX_STORAGE_SIZE.baseException();
+		}
+	}
+
+	/**
+	 * 현재 폴더에서 루트 폴더까지 모든 폴더에 대한 size, updatedAt을 갱신
+	 */
 	private void updateFolderMetadataStatus(FileMetadataDto req, long fileSize, LocalDateTime now) {
 		Long parentFolderId = req.parentFolderId();
 		while (parentFolderId != null) {
@@ -117,6 +124,9 @@ public class S3FileService implements FileService {
 		}
 	}
 
+	/**
+	 * 요청한 parentFolderId가 자신의 폴더에 대한 id인지 확인
+	 */
 	private void validateParentFolder(long parentFolderId, long userId) {
 		FolderMetadata folderMetadata = folderMetadataRepository.findById(parentFolderId)
 			.orElseThrow(ErrorCode.FOLDER_NOT_FOUND::baseException);
@@ -125,6 +135,9 @@ public class S3FileService implements FileService {
 		}
 	}
 
+	/**
+	 * UUID를 생성해 이미 존재하는지 확인
+	 */
 	private String getUuidFileName() {
 		String uuidFileName = UUID.randomUUID().toString();
 		while (fileMetadataRepository.existsByUuidFileName(uuidFileName)) {
