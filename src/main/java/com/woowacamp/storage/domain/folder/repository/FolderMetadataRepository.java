d13c639877e97fcf785e165d64beeab2ef8371bb
kariskan <zkfzpf56@naver.com>
2024-08-16 21:21:17 +0900

    fix: 폴더 조회 시, lock 적용

--- src/main/java/com/woowacamp/storage/domain/folder/repository/FolderMetadataRepository.java
+++ src/main/java/com/woowacamp/storage/domain/folder/repository/FolderMetadataRepository.java
@@ -1,14 +1,15 @@
 package com.woowacamp.storage.domain.folder.repository;
 
-import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.woowacamp.storage.domain.folder.entity.FolderMetadata;
+
+import jakarta.persistence.LockModeType;
 
 public interface FolderMetadataRepository extends JpaRepository<FolderMetadata, Long>, FolderCustomRepository {
 
@@ -20,4 +21,7 @@ public interface FolderMetadataRepository extends JpaRepository<FolderMetadata, 
 			select f.parentFolderId from FolderMetadata f where f.id = :id
 		""")
 	Optional<Long> findParentFolderIdById(@Param("id") Long id);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	Optional<FolderMetadata> findById(long id);
 }
