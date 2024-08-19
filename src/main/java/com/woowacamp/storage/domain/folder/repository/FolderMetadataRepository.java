aaff17489edc7a47406989ea7f1599fb1b3c82f8
kariskan <zkfzpf56@naver.com>
2024-08-19 09:58:30 +0900

    feat: folder 이동 시 validation 추가

--- src/main/java/com/woowacamp/storage/domain/folder/repository/FolderMetadataRepository.java
+++ src/main/java/com/woowacamp/storage/domain/folder/repository/FolderMetadataRepository.java
@@ -3,12 +3,14 @@ package com.woowacamp.storage.domain.folder.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.woowacamp.storage.domain.folder.entity.FolderMetadata;
+
+import jakarta.persistence.LockModeType;
 
 public interface FolderMetadataRepository extends JpaRepository<FolderMetadata, Long>, FolderCustomRepository {
 
@@ -20,4 +22,12 @@ public interface FolderMetadataRepository extends JpaRepository<FolderMetadata, 
 			select f.parentFolderId from FolderMetadata f where f.id = :id
 		""")
 	Optional<Long> findParentFolderIdById(@Param("id") Long id);
+
+	@Query(value = """
+			select f.id from FolderMetadata f where f.parentFolderId = :parentFolderId
+		""")
+	List<Long> findIdsByParentFolderId(@Param("parentFolderId") Long parentFolderId);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	Optional<FolderMetadata> findById(long id);
 }
