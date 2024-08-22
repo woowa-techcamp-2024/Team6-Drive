8081adab18e7d5bc11e7808c4ee26b9cd62fe8a1
김수현 <i960107@naver.com>
2024-08-22 16:49:07 +0900

    feat: SharedLink 엔티티 수정 및 인덱스 추가
    - 사용하지 않는 token필드 제거
    - shareLinkUrl 대신 redirect Url(공유 링크 접속 시 리다이렉트 해줄 url)
    - 이미 생성된 링크 있는지 조회시에 is_file, target_id 조건으로 조회시 속도 높이기 위해 인덱스 추가

--- src/main/java/com/woowacamp/storage/domain/shredlink/repository/SharedLinkRepository.java
+++ src/main/java/com/woowacamp/storage/domain/shredlink/repository/SharedLinkRepository.java
@@ -1,5 +1,6 @@
 package com.woowacamp.storage.domain.shredlink.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import com.woowacamp.storage.domain.shredlink.entity.SharedLink;
 
 public interface SharedLinkRepository extends JpaRepository<SharedLink, Long> {
 	Optional<SharedLink> findBySharedId(String shareId);
+
+	List<SharedLink> findByIsFileAndTargetId(boolean isFile, long targetId);
 }
