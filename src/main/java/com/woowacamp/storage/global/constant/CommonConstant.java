70c2a8a0e10b96cf3971fd4fa599afb12d969a84
kariskan <zkfzpf56@naver.com>
2024-08-22 15:26:01 +0900

    feat: ThumbnailWriter 스레드 풀 관련 상수 추가

--- src/main/java/com/woowacamp/storage/global/constant/CommonConstant.java
+++ src/main/java/com/woowacamp/storage/global/constant/CommonConstant.java
@@ -7,5 +7,10 @@ public class CommonConstant {
 	public static final int FILE_WRITER_MAXIMUM_POOL_SIZE = 40;
 	public static final int FILE_WRITER_KEEP_ALIVE_TIME = 10;
 	public static final int FILE_WRITER_QUEUE_SIZE = 400;
+	public static final int THUMBNAIL_WRITER_CORE_POOL_SIZE = 10;
+	public static final int THUMBNAIL_WRITER_MAXIMUM_POOL_SIZE = 100;
+	public static final int THUMBNAIL_WRITER_KEEP_ALIVE_TIME = 0;
 	public static final int ORPHAN_PARENT_ID = -1;
+	// 1MB
+	public static final int THUMBNAIL_SIZE = 1024 * 1024;
 }
