4b0e556cb2ee2bd71c211a3998778f4bc4243b69
kariskan <zkfzpf56@naver.com>
2024-08-27 01:12:19 +0900

    fix: 스레드풀 대기 큐 사이즈 수정

--- src/main/java/com/woowacamp/storage/global/constant/CommonConstant.java
+++ src/main/java/com/woowacamp/storage/global/constant/CommonConstant.java
@@ -8,7 +8,7 @@ public class CommonConstant {
 	public static final int FILE_WRITER_CORE_POOL_SIZE = 20;
 	public static final int FILE_WRITER_MAXIMUM_POOL_SIZE = 40;
 	public static final int FILE_WRITER_KEEP_ALIVE_TIME = 10;
-	public static final int FILE_WRITER_QUEUE_SIZE = 10;
+	public static final int FILE_WRITER_QUEUE_SIZE = 40;
 	public static final int THUMBNAIL_WRITER_CORE_POOL_SIZE = 10;
 	public static final int THUMBNAIL_WRITER_MAXIMUM_POOL_SIZE = 100;
 	public static final int THUMBNAIL_WRITER_KEEP_ALIVE_TIME = 0;
